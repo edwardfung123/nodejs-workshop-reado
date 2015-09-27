00d364ef1483efa7dcb410106e580ea09658cc89
Edward Fung <edwardfung@redso.com.hk>
2015-09-27 21:55:50 +0800

    update 01-07

--- 01/07_expressjs/answers/v2.js
+++ 01/07_expressjs/answers/v2.js
@@ -4,7 +4,8 @@ var _ = require('underscore');
 
 // Things we learnt from exercise 1.
 // - some functions like get_by_id is called quite often.
-// - better abstract the DB implementation from the API layer.
+// - better abstract the DB implementation from the API layer. The API layer
+//   should not care about how those models' CRUD action are implemented.
 
 // Create an in memory users db (o:
 var User = require('../models/user.js');
@@ -12,27 +13,42 @@ var User = require('../models/user.js');
 router.post('/users', function(req, res, next) {
   // TODO: create a new user in the DB.
   var user = req.body;
+  console.log('user from body');
   console.log(user);
 
   // TODO: check if the user is registered
-  var existingUser = _.find(users, function(u, i){
-    return u.id = user.id;
+  User.get_by_id(user.id, function(err, user){
+    if (user){
+      // User found
+      res.status(400).json({message: 'already registered'});
+      return;
+    }
+
+    if (err.code == User.ERRORS.USER_NOT_FOUND){
+      // So we can save it
+      var newUser = new User(req.body);
+      newUser.save(function(err){
+        res.json(newUser.toJSON());
+      });
+      return;
+    }
+
   });
-
-  if (existingUser){
-    res.status(400).json({'message': 'already registered'});
-    return;
-  }
-
-  users.push(user);
-
-  // Return the user obj back to the caller
-  res.json(user);
 });
 
 router.get('/users', function(req, res, next){
   // TODO: return the list of users.
-  res.json(users);
+  var options = {
+    offset: parseInt(req.query.offset, 10) || 0,
+    hits: parseInt(req.query.hits) || 10,
+  };
+  User.find(options, function(err, users){
+    if (err){
+      res.status(err.code).json({message: err.message});
+      return;
+    }
+    res.json(users);
+  });
 });
 
 router.get('/users/:id', function(req, res, next){
@@ -43,17 +59,15 @@ router.get('/users/:id', function(req, res, next){
     return;
   }
 
-  var user = _.find(users, function(u, i){
-    console.log(u.id);
-    return u.id == uid;
+  User.get_by_id(uid, function(err, user){
+    if (err){
+      res.status(err.code).json({'message': err.message});
+      return;
+    }
+
+    res.json(user);
   });
 
-  if (!user){
-    res.status(404).json({'message': 'user not found'});
-    return;
-  }
-
-  res.json(user);
 });
 
 module.exports = router;
